338ba410239eaf85528cd31670a49af17657f23a
Ray <ray@vernontm.com>
2026-02-20 10:45:39 -0600

    Fix page refresh by adding key to Outlet based on pathname

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 const MainLayout = () => {
+  const location = useLocation()
+  
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
       <main className="flex-1 overflow-auto">
         <div className="p-6">
-          <Outlet />
+          <Outlet key={location.pathname} />
         </div>
       </main>
     </div>
